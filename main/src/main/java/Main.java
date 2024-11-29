999cf8bcfbb676d0442deb7496f9cfc97b04d6a9
KaueSilvaDev <kaue.o.silva35@gmail.com>
2024-11-29 18:39:03 -0300

    implementando logica de herança para as classes pro slack, adicionando dependencias  e os metodos na main

--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import com.slack.api.methods.SlackApiException;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SlackApiException, IOException {
+        ConexaoBd conexao = new ConexaoBd();
+
+//        MensagemError msgErro = new MensagemError();
+//        MensagemRecomendacao msgReco = new MensagemRecomendacao();
+//
+//        msgReco.enviarMensagem();
+//        msgErro.enviarMensagem();
+
+        SqlQuerys.consultarLogsError();
 
     }
 }
